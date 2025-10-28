26bb7104c39b57873edc25b27f4c93c3151e78de
maurelo860@gmail.com <maurelo860@gmail.com>
2025-10-28 11:37:44 -0600

    Se añadio el analizadorLexico por jflex y una prueba basica en el main

--- src/main/java/com/mycompany/analizadores/Analizadores.java
+++ src/main/java/com/mycompany/analizadores/Analizadores.java
@@ -1,8 +1,40 @@
 package com.mycompany.analizadores;
+
+import java.io.StringReader;
+
 public class Analizadores 
 {
     public static void main(String[] args) 
     {
-        System.out.println("Hello World!");
+        // Pruebas
+        String textoEntrada = """
+                              DEFINIR nombre COMO cadena;
+                              // Esto es un comentario gggggg holi
+                              nombre = "HOLA mundo";
+                              
+                              DEFINIR x COMO numero;
+                              x = 5 + 2 * 3;
+                              ESCRIBIR(x + 1); /* Esto es un comentario de bloque */
+                              4545aw145.15 // es Error Lexico 
+                              """;
+        System.out.println("Iniciando Analisis Lexico");
+        StringReader lector = new StringReader(textoEntrada);
+        AnalizadorLexico analizador = new AnalizadorLexico(lector);
+        try
+        {
+            Token tokenActual;
+            do
+            {
+                tokenActual = analizador.siguienteToken();
+                System.out.println(tokenActual.toString());
+            }
+            while(tokenActual.tipo != TipoToken.FIN);
+            System.out.println("Fin del analisis Lexico");
+        }
+        catch (Exception e)
+        {
+            System.out.println("Ocurrio un error durante el analisis");
+            e.printStackTrace();
+        }
     }
 }
